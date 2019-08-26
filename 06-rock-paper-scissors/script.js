12f7a73b0125202bf4b95fbc6ed24f34f64834d6
Monica <monicarigi@hotmail.com>
2019-08-26 19:22:55 +0700

    06-RPS-v3

--- 06-rock-paper-scissors/script.js
+++ 06-rock-paper-scissors/script.js
@@ -1,65 +1,23 @@
 let result = 0
 let options = ['rock', 'paper', 'scissors']
+let outcome = ''
 
 const play = hand => {
 	let resp = Math.round(Math.random()*2)
 
-	let opponent = Array.from(document.getElementById('opponent').getElementsByClassName('option'))
-
-	opponent.forEach(e => {
-		e.classList.add('hidden')
+	Array.from(document.getElementById('opponent').getElementsByClassName('option')).forEach(e => {
+		e.id == options[resp] ? e.classList.remove('hidden') : e.classList.add('hidden')
 	})
-
-	opponent[resp].classList.remove('hidden')
-	let outcome = ''
 
 	if (resp == options.indexOf(hand)) {
 		outcome = 'TIE'
-	} else if (!resp) {
-		if (options.indexOf(hand) == options.length-1) {
-			outcome = 'YOU LOSE'
-			result --
-		} else {
-			outcome = 'YOU WIN'
-			result ++
-		}
+	} else if (options.indexOf(hand) == options.length-1 && !resp || resp > options.indexOf(hand)) {
+		outcome = 'YOU LOSE'
+		result --
 	} else {
-		if (resp > options.indexOf(hand)) {
-			outcome = 'YOU LOSE'
-			result --
-		} else {
-			outcome = 'YOU WIN'
-			result ++
-		}
+		outcome = 'YOU WIN'
+		result ++
 	}
-
-	// if (hand == options[resp].id) {
-	// 	outcome = 'TIE'
-	// } else if (resp == 0) {
-	// 	if (hand == 'paper') {
-	// 		outcome = 'YOU WIN'
-	// 		result ++
-	// 	} else {
-	// 		outcome = 'YOU LOSE'
-	// 		result --
-	// 	}
-	// } else if (resp == 1) {
-	// 	if (hand == 'scissors') {
-	// 		outcome = 'YOU WIN'
-	// 		result ++
-	// 	} else {
-	// 		outcome = 'YOU LOSE'
-	// 		result --
-	// 	}
-	// } else {
-	// 	if (hand == 'rock') {
-	// 		outcome = 'YOU WIN'
-	// 		result ++
-	// 	} else {
-	// 		outcome = 'YOU LOSE'
-	// 		result --
-	// 	}
-	// }
 
 	document.getElementById('outcome').innerHTML = `<div>${result}</div><div>${outcome}</div>`
 
