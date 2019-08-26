00168c4bcd8f77b6590f6a384a638b846d8a3c97
Monica Riera <monica@MacBook-Pro-de-Monica.local>
2019-08-26 17:18:03 +0700

    06-RPS-v2

--- 06-rock-paper-scissors/script.js
+++ 06-rock-paper-scissors/script.js
@@ -1,44 +1,65 @@
 let result = 0
+let options = ['rock', 'paper', 'scissors']
 
 const play = hand => {
 	let resp = Math.round(Math.random()*2)
 
-	let options = Array.from(document.getElementById('opponent').getElementsByClassName('option'))
+	let opponent = Array.from(document.getElementById('opponent').getElementsByClassName('option'))
 
-	options.forEach(e => {
+	opponent.forEach(e => {
 		e.classList.add('hidden')
 	})
 
-	options[resp].classList.remove('hidden')
+	opponent[resp].classList.remove('hidden')
 	let outcome = ''
 
-	if (hand == options[resp].id) {
+	if (resp == options.indexOf(hand)) {
 		outcome = 'TIE'
-	} else if (resp == 0) {
-		if (hand == 'paper') {
-			outcome = 'YOU WIN'
-			result ++
-		} else {
+	} else if (!resp) {
+		if (options.indexOf(hand) == options.length-1) {
 			outcome = 'YOU LOSE'
 			result --
-		}
-	} else if (resp == 1) {
-		if (hand == 'scissors') {
+		} else {
 			outcome = 'YOU WIN'
 			result ++
-		} else {
-			outcome = 'YOU LOSE'
-			result --
 		}
 	} else {
-		if (hand == 'rock') {
-			outcome = 'YOU WIN'
-			result ++
-		} else {
+		if (resp > options.indexOf(hand)) {
 			outcome = 'YOU LOSE'
 			result --
+		} else {
+			outcome = 'YOU WIN'
+			result ++
 		}
 	}
+
+	// if (hand == options[resp].id) {
+	// 	outcome = 'TIE'
+	// } else if (resp == 0) {
+	// 	if (hand == 'paper') {
+	// 		outcome = 'YOU WIN'
+	// 		result ++
+	// 	} else {
+	// 		outcome = 'YOU LOSE'
+	// 		result --
+	// 	}
+	// } else if (resp == 1) {
+	// 	if (hand == 'scissors') {
+	// 		outcome = 'YOU WIN'
+	// 		result ++
+	// 	} else {
+	// 		outcome = 'YOU LOSE'
+	// 		result --
+	// 	}
+	// } else {
+	// 	if (hand == 'rock') {
+	// 		outcome = 'YOU WIN'
+	// 		result ++
+	// 	} else {
+	// 		outcome = 'YOU LOSE'
+	// 		result --
+	// 	}
+	// }
 
 	document.getElementById('outcome').innerHTML = `<div>${result}</div><div>${outcome}</div>`
 
