1ba995290a749e55db34a74e7abf3c8db0f36663
Monica <monicarigi@hotmail.com>
2019-08-29 11:00:32 +0700

    Changes during live coding

--- 05-forms/50502/script.js
+++ 05-forms/50502/script.js
@@ -5,4 +5,7 @@ const sumNum = () => {
 		sum += Number(e.value)
 	})
 	document.getElementById('total').innerHTML = sum
+
+	// 	document.getElementsById('total').innerHTML = Array.from(document.getElementsByClassName('number')).map(e => e.value).reduce((a,b) => a + b)
+	}
 }
--- 06-rock-paper-scissors/script.js
+++ 06-rock-paper-scissors/script.js
@@ -1,24 +1,71 @@
-let result = 0
+// let result = 0
+// let options = ['rock', 'paper', 'scissors']
+// let outcome = ''
+//
+// const toggleOpponent = (resp) => {
+// 	Array.from(document.getElementById('opponent').getElementsByClassName('option')).forEach(e => {
+// 		e.id == options[resp] ? e.classList.remove('hidden') : e.classList.add('hidden')
+// 	})
+// }
+//
+// const checkWinner = (resp, hand) => {
+// 	if (resp == options.indexOf(hand)) {
+// 		outcome = 'TIE'
+// 	} else if (options.indexOf(hand) == options.length-1 && !resp || resp > options.indexOf(hand)) {
+// 		outcome = 'YOU LOSE'
+// 		result --
+// 	} else {
+// 		outcome = 'YOU WIN'
+// 		result ++
+// 	}
+// }
+//
+// const play = hand => {
+// 	let resp = Math.round(Math.random()*2)
+// 	toggleOpponent(resp)
+//
+// 	checkWinner(resp, hand)
+//
+// 	document.getElementById('outcome').innerHTML = `<div>${result}</div><div>${outcome}</div>`
+// }
+// DOM
+
+let opponent = document.getElementById('opponent')
+let outcomeDOM = document.getElementById('outcome')
+
+// DATA
+
 let options = ['rock', 'paper', 'scissors']
 let outcome = ''
+let score = 0
 
-const play = hand => {
-	let resp = Math.round(Math.random()*2)
+// FUNCTIONS
 
-	Array.from(document.getElementById('opponent').getElementsByClassName('option')).forEach(e => {
-		e.id == options[resp] ? e.classList.remove('hidden') : e.classList.add('hidden')
-	})
+const makeRandomChoice = () => {
+	return options[Math.floor(Math.random() * 3)]
+}
 
-	if (resp == options.indexOf(hand)) {
-		outcome = 'TIE'
-	} else if (options.indexOf(hand) == options.length-1 && !resp || resp > options.indexOf(hand)) {
-		outcome = 'YOU LOSE'
-		result --
+const toggleOptions = (choice) => {
+	Array.from(opponent.getElementsByClassName('option')).forEach(o => o.classList.add('hidden'))
+	document.getElementById(choice).classList.remove('hidden')
+}
+
+const checkWinner = (player, opponent) => {
+	if (player === opponent) {
+		outcome = 'tie'
+	} else if (options.indexOf(player) < options.indexOf(opponent) && ( options.indexOf(player) != 0 && options.indexOf(opponent) != options.length - 1 ) || options.indexOf(player) === options.length - 1 && options.indexOf(opponent) == 0) {
+		outcome = 'lose'
+		score--
 	} else {
-		outcome = 'YOU WIN'
-		result ++
+		outcome = 'win'
+		score++
 	}
+	return {outcome, score}
+}
 
-	document.getElementById('outcome').innerHTML = `<div>${result}</div><div>${outcome}</div>`
-
+const play = (playerChoice) => {
+	let opponentChoice = makeRandomChoice()
+	toggleOptions(opponentChoice)
+	let result = checkWinner(playerChoice, opponentChoice)
+	outcomeDOM.innerHTML = `${result.outcome}<br>${result.score}`
 }
